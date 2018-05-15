55c0b347814cc2732fb911b2b081daccc4af34ef
Tim Elfrink <telfrink@mobiquityinc.com>
2018-05-15 15:53:56 +0200

    Update run_chain.sh

--- examples/run_chain.sh
+++ examples/run_chain.sh
@@ -3,7 +3,7 @@
 for i in 10 20 30 40 50 60 70 80 90 100;
 do
     do
-        for exp in bs noisy eps;
+        for exp in eps noisy bs;
         python examples/chain_env.py --n "$i" --exp "$exp" --episodes 2000
     done
 done
