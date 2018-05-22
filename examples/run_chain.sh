84901f773d6a8dc8fea8cc1430a4ab172ba671df
Tim Elfrink <telfrink@mobiquityinc.com>
2018-05-22 09:14:18 +0200

    Update run_chain.sh

--- examples/run_chain.sh
+++ examples/run_chain.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
-for i in 10 20 30 40 50 60 70 80 90 100;
+for seed in 1 2 3
 do
-	for exp in bs noisy eps;
-	do
-        python examples/chain_env.py --n "$i" --exp "$exp" --episodes 2000
+    for i in 10 20 30 40 50 60 70 80 90 100;
+    do
+	    for exp in noisy eps;
+	    do
+            python examples/chain_env.py --n "$i" --exp "$exp" --episodes 2000 --seed "$seed"
+        done
     done
 done
 aws s3 cp ./models/ s3://thesis-tim/models/ --recursive
