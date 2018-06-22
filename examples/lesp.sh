876b3380754d63806899e81ce0a9350aa794fd96
Tim Elfrink <telfrink@mobiquityinc.com>
2018-06-22 09:30:53 +0200

    Create lesp.sh

---
+++ examples/lesp.sh
@@ -0,0 +1,13 @@
+for seed in 1 2 3
+do
+    for envname in BoxingDeterministic-v4;
+    do
+	    for exp in leps;
+	    do
+            python examples/atari_env.py --envname "$envname" --exp "$exp" --seed "$seed"
+        done
+    done
+done
+aws s3 cp ./models/ s3://thesis-tim-files/models/atari/ --recursive
+aws s3 cp ./logs/ s3://thesis-tim-files/logs/atari/ --recursive
+sudo shutdown -P now
